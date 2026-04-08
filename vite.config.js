0091775e3b4027562d17e74adace5fe78dfb0647
JoseCortez1 <jose.vazcortez@gmail.com>
2026-04-08 02:52:13 +0000

    fix(config): add domain to avoid error with vite production

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['deb-tracker.educortez.com'],
+  },
+  preview: {
+    allowedHosts: ['deb-tracker.educortez.com'],
+  },
 });
